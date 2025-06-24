d0b542da47cee7e8484fa7ece16fcdb7b8a128d0
gowthamsmazz <gowthamsmart45@gmail.com>
2025-06-24 17:01:14 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,156 @@
+const logotext = "Gowrishankar";
+const meta = {
+    title: "Gowrishankar",
+    description: "I’m Gowrishankar Senior Web Devleoper _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Gowrishankar",
+    animated: {
+        first: "I'm a Website Developer",
+    },
+    description: "I'm passionate about coding and specialize in developing visually appealing websites.",
+    your_img_url: "/assets/images/IMG_1721.jpg",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I'm a front-end developer with 6.5+ years of experience building responsive websites using HTML, CSS, JavaScript, React, WordPress, and Shopify. I enjoy crafting clean UI and solving real-world problems through code. Passionate about learning and always open to collaboration.",
+};
+const worktimeline = [{
+        jobtitle: "Senior Website Developer",
+        where: "Spectrum Audit and Accounting, UAE",
+        date: "July 2024 - May 2025",
+    },
+    {
+        jobtitle: "Website Developer",
+        where: "Brandstory Solution Pvt Ltd",
+        date: "August 2019 - July 2024",
+    },
+    
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "React JS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Shopify",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Gowthamsmart45@gmail.com",
+    YOUR_FONE: "+91-9500700257",
+    description: "I’m a passionate web developer with a focus on building responsive, user-friendly websites. Whether you’re hiring or looking for a freelance collaborator, feel free to get in touch—I’m open to exciting opportunities.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/gowrishankar-sankaran-85b4b6205/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
